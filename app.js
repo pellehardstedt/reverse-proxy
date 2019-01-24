bbf60790658d59a132a91f2967b3e491dcf32115
Pelle Härdstedt <pelle.hardstedt@gmail.com>
2019-01-24 12:49:06 +0100

    app.js

---
+++ app.js
@@ -0,0 +1,46 @@
+const http = require('http');
+const httpProxy = require('http-proxy');
+
+const proxy = httpProxy.createProxyServer();
+
+proxy.on('error', function(e){
+  console.log("Proxy Error ", new Date(), e);
+})
+
+http.createServer((req,res)=> {
+
+  setResponseHeaders(req,res);
+
+  let host = req.headers.host;
+  console.log(host);
+  let hostParts = host.split('.');
+  let topDomain = hostParts.pop();
+  let domain = hostParts.pop();
+  let subDomain = hostParts.join('.');
+  let urlParts + req.url.split('/');
+
+  let port;
+
+  if(subDomain == '' || subDomain == 'www'){
+    port = 4001;
+  }
+  else if (subDomain == 'example') {
+    port = 3000;
+  }
+  else {
+    res.statusCode = 500;
+    res.end('Can not find you app!');
+  }
+  if(port){
+    proxy.web(req,res,{target:'ht'+'tp://127.0.0.1:' + port});
+  }
+}).listen(80)
+
+function setResponseHeaders(req,res){
+  res.oldWriteHead = res.writeHead;
+
+  res.writeHead = function(statusCode, headers){
+    res.setHeader('x-powered-by','Pelle');
+    res.oldWriteHead(statusCode, headers);
+  }
+}
